9d84ca816dd3b45aff69c5f590348be41ff45ca4
Diego Lopes <diego.lopes@ajcred.com>
2022-10-13 18:14:37 -0300

    estudos ignitelab

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Text } from './components/Text'
+import './styles/global.css'
+
+export function App() {
+
+  return (
+   <>
+   <h1 className="font-bold text-2xl">Hello World</h1>
+   <Text />
+   </>
+  )
+}
+
